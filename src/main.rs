59bc51c3ef28126259d8265d8e6cb96fad357046
Jack Anderson <jaandersonck@gmail.com>
2022-07-26 11:23:08 -0400

    add early layer logic

--- src/main.rs
+++ src/main.rs
@@ -46,123 +46,7 @@ use std::cell::RefCell;
 use keebLib::codes::Keys::*;
 use keebLib::codes::EnumInt;
 use keebLib::codes::extras::________;
-
-fn char_to_keycode(s: String) -> Vec<u8> {
-    let s = s.as_str();
-    let res: Vec<u8> = match s {
-        // Lower Alphabet
-        "a" => vec![KC_A as u8],
-        "b" => vec![KC_B as u8],
-        "c" => vec![KC_C as u8],
-        "d" => vec![KC_D as u8],
-        "e" => vec![KC_E as u8],
-        "f" => vec![KC_F as u8],
-        "g" => vec![KC_G as u8],
-        "h" => vec![KC_H as u8],
-        "i" => vec![KC_I as u8],
-        "j" => vec![KC_J as u8],
-        "k" => vec![KC_K as u8],
-        "l" => vec![KC_L as u8],
-        "m" => vec![KC_M as u8],
-        "n" => vec![KC_N as u8],
-        "o" => vec![KC_O as u8],
-        "p" => vec![KC_P as u8],
-        "q" => vec![KC_Q as u8],
-        "r" => vec![KC_R as u8],
-        "s" => vec![KC_S as u8],
-        "t" => vec![KC_T as u8],
-        "u" => vec![KC_U as u8],
-        "v" => vec![KC_V as u8],
-        "w" => vec![KC_W as u8],
-        "x" => vec![KC_X as u8],
-        "y" => vec![KC_Y as u8],
-        "z" => vec![KC_Z as u8],
-        // Upper Alphabet
-        "A" => vec![KC_LeftShift as u8, KC_A as u8],
-        "B" => vec![KC_LeftShift as u8, KC_B as u8],
-        "C" => vec![KC_LeftShift as u8, KC_C as u8],
-        "D" => vec![KC_LeftShift as u8, KC_D as u8],
-        "E" => vec![KC_LeftShift as u8, KC_E as u8],
-        "F" => vec![KC_LeftShift as u8, KC_F as u8],
-        "G" => vec![KC_LeftShift as u8, KC_G as u8],
-        "H" => vec![KC_LeftShift as u8, KC_H as u8],
-        "I" => vec![KC_LeftShift as u8, KC_I as u8],
-        "J" => vec![KC_LeftShift as u8, KC_J as u8],
-        "K" => vec![KC_LeftShift as u8, KC_K as u8],
-        "L" => vec![KC_LeftShift as u8, KC_L as u8],
-        "M" => vec![KC_LeftShift as u8, KC_M as u8],
-        "N" => vec![KC_LeftShift as u8, KC_N as u8],
-        "O" => vec![KC_LeftShift as u8, KC_O as u8],
-        "P" => vec![KC_LeftShift as u8, KC_P as u8],
-        "Q" => vec![KC_LeftShift as u8, KC_Q as u8],
-        "R" => vec![KC_LeftShift as u8, KC_R as u8],
-        "S" => vec![KC_LeftShift as u8, KC_S as u8],
-        "T" => vec![KC_LeftShift as u8, KC_T as u8],
-        "U" => vec![KC_LeftShift as u8, KC_U as u8],
-        "V" => vec![KC_LeftShift as u8, KC_V as u8],
-        "W" => vec![KC_LeftShift as u8, KC_W as u8],
-        "X" => vec![KC_LeftShift as u8, KC_X as u8],
-        "Y" => vec![KC_LeftShift as u8, KC_Y as u8],
-        "Z" => vec![KC_LeftShift as u8, KC_Z as u8],
-        // Number row
-        "1" => vec![KC_Num1 as u8],
-        "2" => vec![KC_Num2 as u8],
-        "3" => vec![KC_Num3 as u8],
-        "4" => vec![KC_Num4 as u8],
-        "5" => vec![KC_Num5 as u8],
-        "6" => vec![KC_Num6 as u8], 
-        "7" => vec![KC_Num7 as u8],
-        "8" => vec![KC_Num8 as u8],
-        "9" => vec![KC_Num9 as u8],
-        "0" => vec![KC_Num0 as u8],
-        // Number row symbols
-        "!" => vec![KC_LeftShift as u8, KC_Num1 as u8],
-        "@" => vec![KC_LeftShift as u8, KC_Num2 as u8],
-        "#" => vec![KC_LeftShift as u8, KC_Num3 as u8],
-        "$" => vec![KC_LeftShift as u8, KC_Num4 as u8],
-        "%" => vec![KC_LeftShift as u8, KC_Num5 as u8],
-        "^" => vec![KC_LeftShift as u8, KC_Num6 as u8],
-        "&" => vec![KC_LeftShift as u8, KC_Num7 as u8],
-        "*" => vec![KC_LeftShift as u8, KC_Num8 as u8],
-        "(" => vec![KC_LeftShift as u8, KC_Num9 as u8],
-        ")" => vec![KC_LeftShift as u8, KC_Num0 as u8],
-        // Other characters
-        "`" => vec![KC_Grave as u8],
-        "," => vec![KC_Comma as u8],
-        "." => vec![KC_Dot as u8],
-        "/" => vec![KC_Slash as u8],
-        "\'" => vec![KC_Quote as u8],
-        ";" => vec![KC_Semicolon as u8],
-        "\\" => vec![KC_Backslash as u8],
-        "[" => vec![KC_LeftBracket as u8],
-        "]" => vec![KC_RightBracket as u8],
-        "=" => vec![KC_Equal as u8],
-        "-" => vec![KC_Minus as u8],
-        // Other characters shifted
-        "~" => vec![KC_LeftShift as u8, KC_Grave as u8],
-        "<" => vec![KC_LeftShift as u8, KC_Comma as u8],
-        ">" => vec![KC_LeftShift as u8, KC_Dot as u8],
-        "?" => vec![KC_LeftShift as u8, KC_Slash as u8],
-        "\"" => vec![KC_LeftShift as u8, KC_Quote as u8],
-        ":" => vec![KC_LeftShift as u8, KC_Semicolon as u8],
-        "|" => vec![KC_LeftShift as u8, KC_Backslash as u8],
-        "{" => vec![
-            KC_LeftShift as u8,
-            KC_LeftBracket as u8,
-        ],
-        "}" => vec![
-            KC_LeftShift as u8,
-            KC_RightBracket as u8,
-        ],
-        "+" => vec![KC_LeftShift as u8, KC_Equal as u8],
-        "_" => vec![KC_LeftShift as u8, KC_Minus as u8],
-        // "*" => vec![KpAsterisk as u8],
-        " " => vec![KC_Space as u8],
-        // Otherwise it's 0
-        _ => vec![KC_No as u8],
-    };
-    res
-}
+use keebLib::codes::char_to_keycode;
 
 fn keycode_as_int<T: EnumInt>(e: T) -> u8 {
     e.as_int()
@@ -276,9 +160,11 @@ fn main() {
         let mut active_layer = active_layer.borrow_mut();
         *active_layer = l;
     };
-    fn yes(kc: u32, down: bool) {
-        println!("yes");
-    }
+    let is_layer_active = |l: u32| {
+        let active_layer = active_layer.borrow().to_owned();
+        return active_layer == l;
+    };
+
     // TEMPORARILY MUTABLE, in the future it will probably be modified to be immutable and better
     let layer_main = 
        RefCell::new( [
@@ -287,10 +173,8 @@ fn main() {
         KC_Tab.i(),     KC_Q.i(),   KC_W.i(),   KC_E.i(),   KC_R.i(),   KC_T.i(),   KC_Y.i(),   KC_U.i(),   KC_I.i(),   KC_O.i(),   KC_P.i(),    KC_LeftBracket.i(), KC_RightBracket.i(), KC_Backslash.i(),                KC_PageUp.i(),
         KC_CapsLock.i(),KC_A.i(),   KC_S.i(),   KC_D.i(),   KC_F.i(),   KC_G.i(),   KC_H.i(),   KC_J.i(),   KC_K.i(),   KC_L.i(),   KC_Semicolon.i(), KC_Quote.i(), KC_Enter.i(),                                          KC_PageDown.i(),
         KC_LeftShift.i(),  KC_Z.i(),   KC_X.i(),   KC_C.i(),   KC_V.i(),   KC_B.i(),   KC_N.i(),   KC_M.i(),   KC_Comma.i(), KC_Dot.i(),  KC_Slash.i(), KC_RightShift.i(),                 KC_Up.i(),                      KC_End.i(),
-       KC_LeftCtrl.i(), KC_Menu.i(), KC_LeftAlt.i(),                 KC_Space.i(),                            KC_RightAlt.i(), layer_function(yes),   KC_RightCtrl.i(),                 KC_Left.i(), KC_Down.i(), KC_Right.i()
+       KC_LeftCtrl.i(), KC_Menu.i(), KC_LeftAlt.i(),                 KC_Space.i(),                            KC_RightAlt.i(), layer(1),   KC_RightCtrl.i(),                 KC_Left.i(), KC_Down.i(), KC_Right.i()
     ].iter().map(|&e| e as u32).collect::<Vec<u32>>());
-    // TEMPORARILY THERE IS NO ORGANIZED SYSTEM FOR LAYERS
-    // THE FN KEY IS AT INDEX 78, SO WE CAN MODIFY IT TO BE A LAYER activation key to activate layer 1
     let layer_1 = 
         RefCell::new([
         KC_Escape,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_PrintScreen,                                   KC_AudioMute,
@@ -326,6 +210,11 @@ fn main() {
         let key = map[index as usize]; // gets the keycode of the pressed key in the keymap, pressed key is the physical key
         (index, key)
     };
+
+    let run_layer_keys = |kc:u32, down:bool| {
+       
+    };
+
     let mut on_key_down = |kc: u32| -> () {
         // set_key_down(kc, true);
         // never register the physical key, which kc is
@@ -333,87 +222,86 @@ fn main() {
 
         let layer_main = layer_main.borrow().to_owned();
         // Follow keymaps
-        let (index, key) = get_keycode_from_map(kc, &layer_main);
+        let (index, key) = get_keycode_from_map(kc, &km[0]);
         
         
         // This checks if the value of the key is a layer activation key or a function key, both of which are not u8 or keycodes, then if theyre not it will register the key as normal
-        let main_mapped_keycode = layer_main[index as usize]; // this is the keycode of the pressed key in the keymap, kc is the physical key.
+        let main_mapped_keycode = km[0][index as usize]; // this is the keycode of the pressed key in the keymap, kc is the physical key.
         if(main_mapped_keycode > 255) {
             if main_mapped_keycode > 255 && main_mapped_keycode <= 288 {
                 set_key_down(main_mapped_keycode, true);
+
                 // this is a layer key because it's within bounds off 256 and 288
-                let layer_index = &layer_main[index as usize] - 256;
+                let layer_index = &layer_main[index as usize] - 255;
                 let layer = &km[layer_index as usize].to_owned();
                 set_layer(layer_index);
                 
             }
             else if main_mapped_keycode > 288 {
                 set_key_down(main_mapped_keycode, true);
+
                 // this is a function pointer because it's greater than 288
                 let fns = functions.borrow();
                 let fun = fns.get(&main_mapped_keycode).unwrap();
-                // running the function with parameters of the key going down
-                let fns = functions.borrow();
-                fns.get(&main_mapped_keycode).unwrap()(kc, true);
+                fun(kc, true);
             }
         }
-        else {
+        else if is_layer_active(0) {
             // this *should* never panic because above we have checked for the possibilities of the layer key being out of an 8 bit number's bounds
-            set_key_down(kc, true);
             reg_key(layer_main[index as usize].try_into().unwrap());
+            set_key_down(kc, true);
         }
         // Follow layers
-
+        run_layer_keys(kc, true);
     }; 
     let on_key_up = |kc: u32| {
         // set_key_down(kc, false);
 
         let layer_main = layer_main.borrow().to_owned();
         // Follow keymaps
-        let (index, key) = get_keycode_from_map(kc, &layer_main);
+        let (index, key) = get_keycode_from_map(kc, &km[0]);
         // // Unregister layer keys if the layer key is lifted
         // unreg_key(key as u8);
 
         // This checks if the value of the key is a layer activation key or a function key, both of which are not u8 or keycodes, then if theyre not it will register the key as normal
-        let main_mapped_keycode = layer_main[index as usize]; // this is the keycode of the pressed key in the keymap, kc is the physical key.
+        let main_mapped_keycode = km[0][index as usize]; // this is the keycode of the pressed key in the keymap, kc is the physical key.
         if(main_mapped_keycode > 255) {
             if main_mapped_keycode > 255 && main_mapped_keycode <= 288 {
                 set_key_down(main_mapped_keycode, false);
+
                 // this is a layer key because it's within bounds off 256 and 288
-                let layer_index = &layer_main[index as usize] - 256;
+                let layer_index = &km[0][index as usize] - 256;
                 let layer = &km[layer_index as usize].to_owned();
                 set_layer(layer_index);
-                
             }
             else if main_mapped_keycode > 288 {
                 set_key_down(main_mapped_keycode, false);
+
                 // this is a function pointer because it's greater than 288
                 let fns = functions.borrow();
                 let fun = fns.get(&main_mapped_keycode).unwrap();
-                // running the function with parameters of the key going down
-                let fns = functions.borrow();
-                fns.get(&main_mapped_keycode).unwrap()(kc, false);
+                fun(kc, true);
             }
         }
-        else {
+        else if is_layer_active(0){
             // this *should* never panic because above we have checked for the possibilities of the layer key being out of an 8 bit number's bounds
+            unreg_key(layer_main[index as usize].try_into().unwrap());
             set_key_down(kc, false);
-            reg_key(layer_main[index as usize].try_into().unwrap());
         }
 
-        // Soft exit keybind
-        if is_key_down(KC_RollOver as u32) {
-            if key as u8 == KC_Backslash as u8 {
-                let mut exit = exit.borrow_mut();
-                *exit = true;
-                unreg_key(KC_RollOver as u8);
-                unreg_key(KC_Backslash as u8);
-            }
-        }
+        // // Soft exit keybind
+        // if is_key_down(KC_RollOver as u32) {
+        //     if key as u8 == KC_Backslash as u8 {
+        //         let mut exit = exit.borrow_mut();
+        //         *exit = true;
+        //         unreg_key(KC_RollOver as u8);
+        //         unreg_key(KC_Backslash as u8);
+        //     }
+        // }
 
         // Follow layers
         // index is the index of the pressed key in the keymap, pressed key is the physical key, which could differ from the keycode of the pressed key. the index is used to get the physical locaiton of differing keys
-        
+        run_layer_keys(kc, false);
     };
 
     let on_knob_turned = |cw:bool|{
@@ -448,6 +336,7 @@ fn main() {
     // input loop
     loop {
         println!("{:?}", keys_down.borrow());
+        println!("{:?}", active_layer.borrow());
         let exit = exit.borrow().to_owned();
         if exit == true { intercept_mode(0); break; }
         // receiver configuration
